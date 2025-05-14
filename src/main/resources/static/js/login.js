9a1b928611987d1fcd2cdbdc269272772b0d8b10
oono-lab <oonoyusukeda1024@gmail.com>
2025-05-15 01:11:53 +0900

    ログイン画面設計途中

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,10 +1,16 @@
 function toggleForm(target) {
     const loginForm = document.getElementById('loginForm');
-  
+    const signupForm = document.getElementById('signupForm');
     if (target === 'login') {
-      loginForm.classList.add('shrink');
+      loginForm.classList.add('shrink-login');
+      signupForm.classList.remove('shrink-signup');
+      signupForm.classList.add('second-click');
+      
+
     } else if (target === 'signup') {
-      loginForm.classList.remove('shrink');
+      loginForm.classList.remove('shrink-login');
+      signupForm.classList.remove('second-click');
+      signupForm.classList.add('shrink-signup');
     }
   }
   
